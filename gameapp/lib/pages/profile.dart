6efc9a0690910e4585b69db6fc7253c9a76601f9
Jairus <minguajairus@yahoo.com.ph>
2020-10-07 18:24:10 +0800

    finally

--- gameapp/lib/pages/profile.dart
+++ gameapp/lib/pages/profile.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
@@ -6,81 +7,106 @@ import 'home.dart';
 import '../widgets/gamecard.dart';
 import '../class/gamelist.dart';
 
-
 import 'settings.dart';
+
 class Profile extends StatefulWidget {
   @override
   _ProfileState createState() => _ProfileState();
 }
 
-class _ProfileState extends State<Profile> {
-
+class _ProfileState extends State<Profile> with SingleTickerProviderStateMixin {
+  TabController controller;
+  @override
+  void initState() {
+    super.initState();
+    controller = TabController(vsync: this, length: 3);
+  }
 
   @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Theme.of(context).backgroundColor,
-      appBar: AppBar(
         backgroundColor: Theme.of(context).backgroundColor,
-        centerTitle: true,
-        // toolbarHeight: ,
-        title: Text("Jairus Mingua",style: Theme.of(context).textTheme.bodyText2,),
-      ),
-      body: CustomScrollView(
-        slivers: [
-          SliverList(
-            delegate: SliverChildListDelegate(
-              [
-                Container(
-                  margin: EdgeInsets.only(top:50),
-                  height: 100,
-                  width: 100,
-                  decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      image: DecorationImage(
-                        fit: BoxFit.contain,
-                        image: new NetworkImage(
-                            'https://avatars3.githubusercontent.com/u/18586619?s=460&u=30a1c6ecfa750c3ea4b86bdbbdfc87556ecdd164&v=4'),
-                      )),
-                ),
-                Center(
-                  child: Padding(
-                    padding: EdgeInsets.all(20),
-                    child: Text("cipherxz",style: Theme.of(context).textTheme.bodyText1,),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(20),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Column(
-                        children:[
-                          Text("91"),
-                          Text("Favorites",style: Theme.of(context).textTheme.subtitle1,),
-                        ]
-                      ),
-                      Column(
-                        children:[
-                          Text("10"),
-                         Text("Reviews",style: Theme.of(context).textTheme.subtitle1,),
-                        ]
-                      )
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).backgroundColor,
+          centerTitle: true,
+          // toolbarHeight: ,
+          title: Text(
+            "Jairus Mingua",
+            style: Theme.of(context).textTheme.bodyText2,
+          ),
+        ),
+        body: CustomScrollView(slivers: <Widget>[
+          SliverPersistentHeader(
+              delegate: ProfileView(maxExtent: 250, minExtent: 150)),
+          SliverFillRemaining(
+            child: DefaultTabController(
+              length: 3,
+              child: Scaffold(
+                appBar: AppBar(
+                  
+                  title: TabBar(
+                    tabs: [
+                      Tab(icon: Icon(Icons.directions_car)),
+                      Tab(icon: Icon(Icons.directions_transit)),
+                      Tab(icon: Icon(Icons.directions_bike)),
                     ],
                   ),
-                )
-              ]
+                  
+                ),
+                body: TabBarView(
+                  children: [
+                    Icon(Icons.directions_car),
+                    Icon(Icons.directions_transit),
+                    Icon(Icons.directions_bike),
+                  ],
+                ),
+              ),
             ),
           ),
-          SliverAppBar(
-            
+        ]));
+  }
+}
+
+class ProfileView extends SliverPersistentHeaderDelegate {
+  ProfileView({this.minExtent, @required this.maxExtent});
+  final double minExtent;
+  final double maxExtent;
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return Stack(
+      fit: StackFit.expand,
+      children: [
+        Positioned(
+          left: 16.0,
+          right: 16.0,
+          bottom: 16.0,
+          child: Text(
+            'Lorem ipsum',
+            style: TextStyle(
+              fontSize: 32.0,
+              color: Colors.white.withOpacity(titleOpacity(shrinkOffset)),
+            ),
           ),
-          SliverList(delegate: SliverChildListDelegate([
-           
-          ]))
-        ],
-      ),
+        ),
+      ],
     );
+  }
+
+  double titleOpacity(double shrinkOffset) {
+    // simple formula: fade out text as soon as shrinkOffset > 0
+    return 1.0 - max(0.0, shrinkOffset) / maxExtent;
+    // more complex formula: starts fading out text when shrinkOffset > minExtent
+    //return 1.0 - max(0.0, (shrinkOffset - minExtent)) / (maxExtent - minExtent);
+  }
+
+  @override
+  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) {
+    return true;
   }
 }
