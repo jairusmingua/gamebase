47ddf4611676040fc77fca50077b2cf8f81b6aa0
jairusmingua <minguajairus@yahoo.com.ph>
2020-11-11 09:02:09 +0000

    Deploying to gh-pages from  @ 6df01815b1b3bba8999ff4d8568b6945b97e21ab 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -14,8 +14,8 @@ const RESOURCES = {
 "manifest.json": "43bd0c21a41dba97441bebd389b61abd",
 "main.dart.js": "b471415f685c5aa979f4cb6053c9b8b7",
 "version.json": "42b89e98c22b15c7bcb7d8c65f4a5af3",
-"index.html": "91b3b3efc08bdca59ec4e8391fe0638e",
-"/": "91b3b3efc08bdca59ec4e8391fe0638e",
+"index.html": "3f8c0cfed77b69159ae663f648531871",
+"/": "3f8c0cfed77b69159ae663f648531871",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1"
 };
